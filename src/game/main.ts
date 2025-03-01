f5ad5d27632573f2ff9c92aa3dd4ad8d089f2293
akiful-islam <arian.islam024@gmail.com>
2025-03-01 14:35:52 +0600

    update style and make canvas responsive

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,31 +1,34 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
-import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
-import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
+import { AUTO, Game } from "phaser";
+import { Boot } from "./scenes/Boot";
+import { Game as MainGame } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import { MainMenu } from "./scenes/MainMenu";
+import { Preloader } from "./scenes/Preloader";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+
+const resolution = {
+    width: 720,
+    height: 1280,
+};
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+    width: resolution.width,
+    height: resolution.height,
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    parent: "game-container",
+    backgroundColor: "#028af8",
+    scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
 };
 
 const StartGame = (parent: string) => {
-
     return new Game({ ...config, parent });
-
-}
+};
 
 export default StartGame;
+
