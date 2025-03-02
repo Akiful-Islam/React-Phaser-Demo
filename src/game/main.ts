4533e19403efcb917b300496984324d6ecc3fb6a
akiful-islam <arian.islam024@gmail.com>
2025-03-02 15:25:40 +0600

    add design in meny

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,5 +1,5 @@
 import { Game } from "phaser";
-import { config } from "../configs/game-config";
+import { config } from "./configs/game-config";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
--- src/game/scenes/Menu.ts
+++ src/game/scenes/Menu.ts
@@ -1,5 +1,5 @@
 import { Scene } from "phaser";
-import { buttonConfig } from "../../configs/button-config";
+import { buttonConfig } from "../configs/button-config";
 
 export class Menu extends Scene {
     button: HTMLButtonElement;
@@ -13,6 +13,13 @@ export class Menu extends Scene {
     }
     preload() {
         this.load.image("sky", "assets/sky.png");
+        this.load.spritesheet("dude", "assets/dude.png", {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
+        this.load.image("ground", "assets/platform.png");
+        this.load.image("star", "assets/star.png");
+        this.load.image("bomb", "assets/bomb.png");
 
         const { buttonText, buttonStyle } = buttonConfig;
         this.button = document.createElement("button");
@@ -29,11 +36,29 @@ export class Menu extends Scene {
     create() {
         this.add.image(360, 640, "sky").scaleY = 2.2;
 
+        this.add.text(120, 250, "Click to Start", {
+            fontSize: "3.5rem",
+            color: "#FFFFFF",
+            fontStyle: "bold",
+        });
+
         this.add
-            .dom(360, 600, this.button)
+            .dom(360, 700, this.button)
             .addListener("click")
             .once("click", () => {
                 this.scene.start("Game");
             });
+
+        this.add.image(360, 1100, "ground").setScale(10);
+        this.add.image(360, 915, "dude");
+
+        for (let i = 0; i < 11; i++) {
+            if (i === 5) {
+                this.add.image(110 + i * 50, 500, "bomb");
+                continue;
+            }
+
+            this.add.image(110 + i * 50, 500, "star");
+        }
     }
 }
