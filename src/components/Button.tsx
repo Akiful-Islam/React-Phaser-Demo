1ac8b30440dc2ff7f6c7270e727240a016738cc8
akiful-islam <arian.islam024@gmail.com>
2025-03-02 15:42:47 +0600

    Final comments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,7 @@ import { buttonConfig } from "../game/configs/button-config";
 
 // I wanted to try and implement a react button component inside phaser canvas
 // But I couldn't really figure it out
+// So this component is unused and dead code
 
 const Button = () => {
     const { buttonText, buttonStyle } = buttonConfig;
